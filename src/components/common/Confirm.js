e63bdfd90697c5e558e1805da5be75dca6a54952
mekyleh <kyleholt@Kyles-MacBook-Pro.local>
2018-01-13 14:49:20 -0800

    adding basic modal component:

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,8 +3,26 @@ import { Modal, Text, View } from 'react-native';
 import { Button } from './Button';
 import { CardSection } from './CardSection';
 
-const Confirm = () =>{
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  return (
+    <Modal
+      animationType="slide"
+      onRequestClose={() => {}}
+      transparent
+      visible={visible}
+    >
+      <View>
+        <CardSection>
+          <Text>{children}</Text>
+        </CardSection>
 
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
 };
 
 export { Confirm };
